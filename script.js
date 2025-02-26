9b7d5d545610c342fcc5a58b4ea9c66cd0e61117
Askan-A <aritrasonty@gmail.com>
2025-02-26 12:28:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function showWasteDetails() {
+    var details = document.getElementById("waste-details");
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
